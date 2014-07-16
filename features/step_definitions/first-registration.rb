8538a5308d6ac0ce6d2afb8726ef7d72ab896a47
lrgeofflewis <LRGeoff@Geoffs-MacBook-Pro.local>
2014-07-16 08:55:37 +0100

    entering form pages of first registration

--- features/step_definitions/first-registration.rb
+++ features/step_definitions/first-registration.rb
@@ -1,78 +1,95 @@
 Given(/^I have received an application for a first registration$/) do
   $data = Hash.new()
+  $data['titleNumber'] = 'TEST1234'
+  $data['propertyHouseNumber'] = '3'
+  $data['propertyRoad'] = 'The road'
+  $data['propertyTown'] = 'Plymouth'
+  $data['propertyPostcode'] = 'PL1 6GH'
+  $data['pricePaid'] = '300453'
   $data['forename1'] = 'Geoff'
   $data['surname1'] = 'Lewis'
+  $data['forename2'] = 'Andy'
+  $data['surname2'] = 'Moore'
 end
 
 Given(/^I am on the first registration entry screen$/) do
-  visit('http://www.google.com')
+  visit('http://0.0.0.0:8004')
+  #temporarily enter a title number until it generates itself
+  #page.save_screenshot "sshot-#{Time.new.to_i}.png"
+  fill_in('titleNumber', :with => $data['titleNumber'])
 end
 
 When(/^I enter a Property Address$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in('houseNumber', :with => $data['propertyHouseNumber'])
+  fill_in('road', :with => $data['propertyRoad'])
+  fill_in('town', :with => $data['propertyTown'])
+  fill_in('postcode', :with => $data['propertyPostcode'])
 end
 
 When(/^I choose a tenure of Freehold$/) do
-  pending # express the regexp above with the code you wish you had
+  choose('freehold')
 end
 
 When(/^I select class of Absolute$/) do
-  pending # express the regexp above with the code you wish you had
+  choose('absolute')
 end
 
 When(/^I enter a valid price paid$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in('pricePaid', :with => $data['pricePaid'])
 end
 
 When(/^I enter 1 proprietor$/) do
-  fill_in('forename????', :with => $data['forename1'])
-  fill_in('surename????', :with => $data['surname1'])
+  fill_in('firstName1', :with => $data['forename1'])
+  fill_in('surname1', :with => $data['surname1'])
 end
 
 When(/^I enter 2 proprietors$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in('firstName1', :with => $data['forename1'])
+  fill_in('surname1', :with => $data['surname1'])
+  fill_in('firstName2', :with => $data['forename2'])
+  fill_in('surname2', :with => $data['surname2'])
 end
 
 When(/^I Register the transaction$/) do
-  pending # express the regexp above with the code you wish you had
+  click_button('Register')
 end
 
 Then(/^the first registration is registered$/) do
-  pending # express the regexp above with the code you wish you had
+  find(".//*[@id='success']")
 end
 
 When(/^I select class of Good$/) do
-  pending # express the regexp above with the code you wish you had
+  choose('good')
 end
 
 When(/^I select class of Possessory$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I enter no proprietors$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  choose('possessory')
 end
 
 When(/^I select class of Qualified$/) do
-  pending # express the regexp above with the code you wish you had
+  choose('qualified')
 end
 
 When(/^I enter an invalid price paid$/) do
-  pending # express the regexp above with the code you wish you had
+  $invalidPricePaid = "@£$%^&*Broken10"
+  fill_in('pricePaid', :with => $invalidPricePaid)
 end
 
 Then(/^an error page will be displayed$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I am on the first registration entry screen$/) do
-  pending # express the regexp above with the code you wish you had
+  find(".//*[@id='error']")
 end
 
 Then(/^a Title Number is displayed$/) do
-  pending # express the regexp above with the code you wish you had
+  page.save_screenshot "sshot-#{Time.new.to_i}.png"
+  assert_no_selector(".//*[@id='titleNumber']", text: '')
+  #find(".//*[@id='titleNumber' and text()='']")
+  #assert_no_selector ('titleNumber')
+  #if find(".//*[@id='titleNumber']").text =="" then
+#    raise "There is no titleNumber!"
+#  end
 end
 
 Then(/^Title Number is formatted correctly$/) do
-  pending # express the regexp above with the code you wish you had
+  $titleNumber = find(".//*[@id='titleNumber']").text
+  #need to check format here
 end
