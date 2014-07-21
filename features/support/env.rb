9d5870eb4d03d4c7a98775381024168ba4ffbdba
lrgeofflewis <LRGeoff@Geoffs-MacBook-Pro.local>
2014-07-21 11:15:09 +0100

    env change

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,4 +3,4 @@ require 'capybara/cucumber'
 require 'capybara/poltergeist'
 require 'selenium-webdriver'
 
-$CASEWORK_FRONTEND_URL = (ENV['CASEWORK_FRONTEND_URL'] || 'http://0.0.0.0:8004/')
+$CASEWORK_FRONTEND_URL = (ENV['CASEWORK_FRONTEND_URL'] || 'http://0.0.0.0:8004/registration')
