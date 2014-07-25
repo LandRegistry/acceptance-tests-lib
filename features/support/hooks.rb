b36b5188ef1c55faa0afab44e2dc854d8aacceed
mooreandrew <andrewmoore@Andrews-MacBook-Pro.local>
2014-07-25 15:45:04 +0100

    Changed the location of the basic auth code

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,11 @@
-Before do
-
+Before do | scenario |
+  if page.driver.respond_to?(:basic_auth)
+    page.driver.basic_auth($http_auth_name, $http_auth_password)
+  elsif page.driver.respond_to?(:basic_authorize)
+    page.driver.basic_authorize($http_auth_name, $http_auth_password)
+  elsif page.driver.respond_to?(:browser) && page.driver.browser.respond_to?(:basic_authorize)
+    page.driver.browser.basic_authorize($http_auth_name, $http_auth_password)
+  end
 end
 
 
