8968226e2d53cc3354e6a26574f43265188f81c9
Andrew Moore <mooreandrewlr@gmail.com>
2014-08-04 15:40:02 +0000

    Updated run_tests.sh for vagrant image

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+cd ../service-frontend
+./create-user-for-integration-tests.sh > /vagrant/logs/acceptance-tests.log 2>&1
+cd ../casework-frontend
+./create-user-for-integration-tests.sh  >> /vagrant/logs/acceptance-tests.log 2>&1
+
+cd ../acceptance-tests
+
+
 bundle install
 
 if [ -z "$1" ]
