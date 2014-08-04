c5133cd2ac66657de19807cc5940d4a0d59c2da1
Andy Moore <andy.moore@digital.landregistry.gov.uk>
2014-08-04 18:45:29 +0100

    updated run_tests.sh
    Added check in run_tests.sh to see if running in vagrant or not

--- run_tests.sh
+++ run_tests.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-cd ../service-frontend
-./create-user-for-integration-tests.sh > /vagrant/logs/acceptance-tests.log 2>&1
-cd ../casework-frontend
-./create-user-for-integration-tests.sh  >> /vagrant/logs/acceptance-tests.log 2>&1
-
-cd ../acceptance-tests
-
+if [ -f /vagrant/logs ];
+then
+  cd ../service-frontend
+  ./create-user-for-integration-tests.sh > /vagrant/logs/acceptance-tests.log 2>&1
+  cd ../casework-frontend
+  ./create-user-for-integration-tests.sh  >> /vagrant/logs/acceptance-tests.log 2>&1
+  cd ../acceptance-tests
+fi
 
 bundle install
 
