8a1672da7cb0d06653e15cca88f9cafce9791011
Nicolas Devetak <nicolasdevetak@gds3619.local>
2014-08-18 15:25:27 +0100

    screens changed so just updating implementation

--- features/citizen/private-change-of-name.feature
+++ features/citizen/private-change-of-name.feature
@@ -9,7 +9,7 @@ And I am logged in
 When I enter a new name
 And I enter my date of marriage
 And I enter my partners name
-And I enter a Country of marriage
+And I enter UK as the Country of marriage
 And I enter a location of marriage
 And I enter a Marriage Certificate Number
 And I submit the marriage change of name details
@@ -24,7 +24,7 @@ And I am logged in
 When I enter a new name
 And I enter my date of marriage
 And I enter my partners name
-And I enter a Country of marriage
+And I enter UK as the Country of marriage
 And I enter a location of marriage
 And I enter a Marriage Certificate Number
 And I submit the marriage change of name details
@@ -37,7 +37,7 @@ And I have private citizen login credentials
 And I want to request I change my name on the register
 And I am logged in
 When I submit the marriage change of name details
-Then a "This field is required." message for "error_proprietor_new_name" is returned
+Then a "This field is required." message for "error_proprietor_new_surname" is returned
 And a "This field is required." message for "error_partner_name" is returned
 And a "This field is required." message for "error_marriage_date" is returned
 And a "This field is required." message for "error_marriage_place" is returned
--- features/step_definitions/change-of-name.rb
+++ features/step_definitions/change-of-name.rb
@@ -13,11 +13,14 @@ Given(/^I have have got married and I want to change my name on the register$/) 
 end
 
 Given(/^I want to request I change my name on the register$/) do
-  visit("#{$SERVICE_FRONTEND_DOMAIN}/property/#{$regData['title_number']}/edit")
+  #visit("#{$SERVICE_FRONTEND_DOMAIN}/property/#{$regData['title_number']}/edit")
+  visit("#{$SERVICE_FRONTEND_DOMAIN}/property/#{$regData['title_number']}/edit/title.proprietor.1")
+  puts "#{$SERVICE_FRONTEND_DOMAIN}/property/#{$regData['title_number']}/edit/title.proprietor.1"
+
 end
 
 When(/^I enter a new name$/) do
-  fill_in('proprietor_new_name', :with => $data['newName'])
+  fill_in('proprietor_new_surname', :with => $data['newName'])
 end
 
 When(/^I enter my partners name$/) do
@@ -28,8 +31,9 @@ When(/^I enter my date of marriage$/) do
   fill_in('marriage_date', :with => $data['dateOfMarriage'])
 end
 
-When(/^I enter a Country of marriage$/) do
-  fill_in('marriage_country', :with => $data['countryOfMarriage'])
+When(/^I enter UK as the Country of marriage$/) do
+  #fill_in('marriage_country', :with => $data['countryOfMarriage'])
+  choose('United Kingdom')
 end
 
 When(/^I enter a location of marriage$/) do
@@ -53,16 +57,16 @@ When(/^I submit the marriage change of name details$/) do
 end
 
 Then(/^I am presented to certify my details$/) do
-  assert_match('I hereby certify that', page.body, 'Expected to certify statement including personnal details')
+  assert_match('Confirm', page.body, 'Expected to certify statement including personnal details')
   assert_match($data['newName'], page.body, 'Expected to certify statement including personnal details')
   assert_match($data['dateOfMarriage'], page.body, 'Expected to certify statement including personnal details')
   assert_match($data['locationOfMarriage'], page.body, 'Expected to certify statement including personnal details')
-  assert_match($data['countryOfMarriage'], page.body, 'Expected to certify statement including personnal details')
+  #assert_match($data['countryOfMarriage'], page.body, 'Expected to certify statement including personnal details')
   assert_match($data['partnerFullName'], page.body, 'Expected to certify statement including personnal details')
 end
 
 Then(/^I receive a confirmation that my change of name request has been lodged$/) do
-  assert_match('Acknowledgement', page.body, 'Expected to Acknowledgement message')
+  assert_match('Application complete', page.body, 'Expected to Acknowledgement message')
 end
 
 When(/^I accept the certify statement$/) do
