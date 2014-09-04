cd41a840bc1f11ba27ee2c6f44db0670b928abac
Andy Moore <andy.moore@digital.landregistry.gov.uk>
2014-09-04 14:49:24 +0100

    Title Number Check

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,4 +1,4 @@
-def validate_title_number(title_number)
+def validate_title_number(titleNumber)
   assert_equal titleNumber[0,4], 'TEST', 'Title does not have a prefix of TEST'
   assert_equal titleNumber[4,titleNumber.size - 1], titleNumber[4,titleNumber.size - 1].to_i.to_s, 'The title number is not numberic'
   assert_operator titleNumber[4,titleNumber.size - 1].to_i, :>=, 1, 'The number is less than 0'
