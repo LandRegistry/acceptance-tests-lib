d0e49a5b5f56daa9b59001246083e32bf94ba971
Andy Moore <andy.moore@digital.landregistry.gov.uk>
2014-09-17 16:07:36 +0100

    Added Performance Graph

--- features/performance.feature
+++ features/performance.feature
@@ -4,9 +4,9 @@ Scenario: Private Register with Title Extents
   Given I want to run a performance
   And I have the following scenarios
     | SCENARIO                                                               | USERS |
-    | Processing Absolute Freehold First Registration with 1 proprietor (3)  | 2     |
+    | Processing Absolute Freehold First Registration with 1 proprietor (3)  | 1     |
     | view freehold register as citizen                                      | 5     |
-  And I run for 10 minutes
+  And I run for 1 minute
   And I ramp up 1 user every 1 seconds
   When I run the performance test
   Then I expect less than 10 failures
--- features/step_definitions/performance.rb
+++ features/step_definitions/performance.rb
@@ -29,9 +29,6 @@ Given(/^I want to run a performance$/) do
     end
   end
 
-
-
-
 end
 
 Given(/^I have the following scenarios$/) do |table|
@@ -53,16 +50,18 @@ Given(/^I have the following scenarios$/) do |table|
       for i in 0..value[1].to_i - 1
         $vuser_scenarios << $features_hash[value[0]]
       end
+
+      require_relative 'performance/' + value[0].gsub('(','').gsub(')', '').gsub(/ /, '_').capitalize.downcase
+
     end
   end
 
   $vuser_scenarios.shuffle
 
-  puts $running_scenarios_hash
 end
 
-Given(/^I run for (\d+) minutes$/) do |duration|
-  $duration = duration.to_i
+Given(/^I run for (\d+) minute(s|)$/) do |duration, word|
+  $duration = duration.to_i * 60
 end
 
 Given(/^I ramp up (\d+) user every (\d+) seconds$/) do |rampup_users, rampup_time|
@@ -78,28 +77,153 @@ When(/^I run the performance test$/) do
   $results2 = Hash.new()
 
   $results_transactions = Hash.new()
-
   $results_scenarios = Hash.new()
+
+  $results_transactions_graph = Hash.new()
+  $results_scenarios_graph = Hash.new()
+
+  $running_v_users = 0
+
+  $results_vusers = Hash.new()
+
+  $scriptdelaytime = 0
 
   $starttime = Time.new.to_i
 
   $total_failures = 0
 
+  $max_x = 3
+
   puts 'Start Time: ' + $starttime.to_s
-
-
-
-  #Thread.abort_on_exception = true
 
   threads = Array.new()
 
-  #threads.push(Thread.new{loadtest('features/citizen/view_property_details.feature:4')})
+  list_of_tests = Array.new()
+
+
+
+  $vuser_scenarios.each do |value|
+    found = false
+    for i in 0..(list_of_tests.count - 1)
+      if (list_of_tests[i] == value) then
+        found = true
+      end
+    end
+    if (found == false) then
+      list_of_tests << value
+    end
+  end
 
   for i in 0..($amount_of_users - 1)
 
     threads.push(Thread.new{loadtest($vuser_scenarios[i])})
-    sleep($ramp_up_time)
+
   end
+
+
+  File.open('graph/results.js', 'w') { |file| file.write('') }
+
+  cur_time = 0
+
+  running = true
+
+  while (((Time.new.to_i) < ($starttime + $duration)) || ($running_v_users > 0) || (running == true)) do
+
+    cur_time = cur_time + 1
+
+    graph_time = $duration
+    if (cur_time > $duration) then
+      graph_time = cur_time
+    end
+
+  	logfile = ''
+
+    for i in 1..10
+
+      logfile  = logfile + "var graph_data#{i} = []; \n"
+
+    end
+
+  	logfile = logfile + "var graph_xmax = " + (graph_time * 1.05).ceil.to_s + "; \n"
+  	logfile  = logfile + "var graph_ymax = 1; \n"
+  	logfile  = logfile + "var graph_ymax = " + ($amount_of_users * 1.2).ceil.to_s + "; \n"
+  	logfile  = logfile + "graph_data1[0] = 0; \n"
+
+  	for i in 0..((Time.new.to_i - $starttime + 1))
+
+  		if ($results_vusers[i + 1].nil? == true) then
+
+  			$results_vusers[i + 1] = $running_v_users
+
+  		end
+
+  		logfile  = logfile + 'graph_data1[' + i.to_s + '] = "' + $results_vusers[i + 1].to_s + '" ' + ";\n"
+
+
+      num = 1
+      $results_scenarios_graph.each do |key, results2|
+        num = num + 1
+    		if (results2[i + 1].nil? == false) then
+
+    			sum = 0
+    			results2[i + 1].each { |a| sum+=a }
+
+    			logfile  = logfile + 'graph_data' + num.to_s + '[' + i.to_s + '] = "' + (sum / results2[i + 1].size.to_f).round(2).to_s + '" ' + ";\n"
+    			logfile  = logfile + "var graph_y2max = " + ($max_x * 1.1).ceil.to_s + " \n"
+    		end
+
+      end
+
+  	end
+
+    logfile  = logfile + "var graph_details_name = []; \n"
+    logfile  = logfile + "var graph_details_min = []; \n"
+    logfile  = logfile + "var graph_details_max = []; \n"
+    logfile  = logfile + "var graph_details_avg = []; \n"
+
+    for i in 0..(list_of_tests.count - 1)
+
+
+      logfile  = logfile + "graph_details_name[#{i}] = '#{list_of_tests[i]}' \n"
+
+      if (!$results_scenarios[list_of_tests[i]].nil?) then
+
+        logfile  = logfile + "graph_details_max[#{i}] = '#{$results_scenarios[list_of_tests[i]].max}' \n"
+        logfile  = logfile + "graph_details_min[#{i}] = '#{$results_scenarios[list_of_tests[i]].min}' \n"
+        logfile  = logfile + "graph_details_avg[#{i}] = '#{($results_scenarios[list_of_tests[i]].inject{ |sum, el| sum + el }.to_f / $results_scenarios[list_of_tests[i]].size)}' \n"
+
+      else
+
+        logfile  = logfile + "graph_details_max[#{i}] = '0' \n"
+        logfile  = logfile + "graph_details_min[#{i}] = '0' \n"
+        logfile  = logfile + "graph_details_avg[#{i}] = '0' \n"
+
+      end
+
+    end
+
+
+
+
+
+
+
+          if ($running_v_users == 0) then
+              running = false
+          else
+              running = true
+          end
+
+  	File.open('graph/results.js', 'w') { |file| file.write(logfile) }
+
+  	sleep(1)
+
+  end
+
+
+
+
+
 
   for i in 0..($amount_of_users - 1)
      threads[i].join
@@ -110,53 +234,25 @@ end
 
 Then(/^I expect less than (\d+) failures$/) do |failures|
 
-
-  for i in 0..($duration - 1)
-
-  	if ($results2[i + 1].nil? == false) then
-
-  		sum = 0
-  		$results2[i + 1].each { |a| sum+=a }
-
-  		puts 'graph_data[' + i.to_s + '] = "' + (sum / $results2[i + 1].size.to_f).round(2).to_s + '" ' + "\n"
-
-  	end
-
-  end
-
-  sum = 0
-  $results.each { |a| sum+=a }
-
-  puts 'Max Time: ' + $results.max.to_s
-  puts 'Min Time: ' + $results.min.to_s
-  puts 'Avg Time: ' + (sum / $results.size.to_f).to_s
-
-  puts $results2
-
-  puts $results_transactions
-  puts $results_scenarios
-
   assert_operator $total_failures, :<, failures.to_i, 'The failures weren\'t below the threshold'
-
 
 end
 
 Then(/^the scenarios response times were below:$/) do |table|
 
-
   table.raw.each do |value|
-    if (!$features_hash[value[0]].nil?)
-      $amount_of_users = $amount_of_users + value[1].to_i
-      $running_scenarios_hash[$features_hash[value[0]]] = value[1].to_i
-      for i in 0..value[1].to_i - 1
-        $vuser_scenarios << $features_hash[value[0]]
-      end
+    if (value[0] != 'SCENARIO')
+
+      puts value[0]
+      puts value[1]
+      puts $features_hash[value[0]]
+
+      puts value[0] + ' Actual Average: ' + ($results_scenarios[$features_hash[value[0]]].inject{ |sum, el| sum + el }.to_f / $results_scenarios[$features_hash[value[0]]].size).to_s
+
+      assert_operator ($results_scenarios[$features_hash[value[0]]].inject{ |sum, el| sum + el }.to_f / $results_scenarios[$features_hash[value[0]]].size), :<, value[1].to_i, 'The average response time wasn\'t below the threshold'
     end
   end
 
-
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
 end
 
 
@@ -164,13 +260,19 @@ end
 
 def loadtest(cucumber_scenario)
 
+	$scriptdelaytime = $scriptdelaytime + $ramp_up_time
+
+	sleep $scriptdelaytime
+
+
   $stdout.puts cucumber_scenario
   puts cucumber_scenario
 
     scenario_name = $running_scenarios_hash_name[cucumber_scenario].gsub('(','').gsub(')', '').gsub(/ /, '_').capitalize
 
-    require_relative 'performance/' + scenario_name.downcase
     script = Module.const_get(scenario_name).new
+
+    $running_v_users = $running_v_users + 1
 
   	while ((Time.new.to_i)  < ($starttime + $duration)) do
 
@@ -184,28 +286,33 @@ def loadtest(cucumber_scenario)
         $total_failures = $total_failures + 1
       end
 
-
     	script_duration = Time.new.to_i - scriptstart_time
 
-      if ($results_scenarios[cucumber_scenario].nil?) then
-
-        $results_scenarios[cucumber_scenario] = {}
+      if (script_duration > $max_x) then
+        $max_x = script_duration
       end
+
+      if ($results_scenarios[cucumber_scenario].nil?) then
+        $results_scenarios[cucumber_scenario] = []
+        $results_scenarios_graph[cucumber_scenario] = {}
+      end
+
+      $results_scenarios[cucumber_scenario] << script_duration
 
       current_time_id = $duration - (($starttime + $duration) - Time.new.to_i) + 1
 
-      if($results_scenarios[cucumber_scenario][current_time_id].nil? == true) then
-
-        $results_scenarios[cucumber_scenario][current_time_id] = Array.new()
-
+      if($results_scenarios_graph[cucumber_scenario][current_time_id].nil? == true) then
+        $results_scenarios_graph[cucumber_scenario][current_time_id] = Array.new()
       end
 
-      $results_scenarios[cucumber_scenario][current_time_id].push(script_duration)
+      $results_scenarios_graph[cucumber_scenario][current_time_id].push(script_duration)
 
       sleep(1)
-      puts $results2
 
   	end
+
+    $running_v_users = $running_v_users - 1
+
 
 end
 
@@ -224,30 +331,28 @@ def end_traction(step_name, start_time)
   transaction_duration = Time.new.to_i - start_time
 
   if ($results_transactions[step_name].nil?) then
-
-    $results_transactions[step_name] = {}
+    $results_transactions[step_name] = []
+    $results_transactions_graph[step_name] = {}
   end
+
+  $results_transactions[step_name] << transaction_duration
 
   current_time_id = $duration - (($starttime + $duration) - Time.new.to_i) + 1
 
-  if($results_transactions[step_name][current_time_id].nil? == true) then
-
-    $results_transactions[step_name][current_time_id] = Array.new()
-
+  if($results_transactions_graph[step_name][current_time_id].nil? == true) then
+    $results_transactions_graph[step_name][current_time_id] = Array.new()
   end
 
-  $results_transactions[step_name][current_time_id].push(transaction_duration)
+  $results_transactions_graph[step_name][current_time_id].push(transaction_duration)
 
 end
 
 def http_get(url)
 
-  $stdout.puts url
+  #$stdout.puts url
 
 	uri = URI.parse(url)
-
 	http = Net::HTTP.new(uri.host, uri.port)
-
 	request = Net::HTTP::Get.new(uri.request_uri)
 	response = http.request(request)
 
@@ -255,12 +360,10 @@ end
 
 def http_post(url, data)
 
-  $stdout.puts url
+  #$stdout.puts url
 
   uri = URI.parse(url)
-
   http = Net::HTTP.new(uri.host, uri.port)
-
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
 
