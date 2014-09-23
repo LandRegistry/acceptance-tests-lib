b2370e6f594cf9be1cedee7ef55969e23d6161c3
Carrie1910 <carrie.sutton@digital.landregistry.gov.uk>
2014-09-23 09:18:03 +0100

    Initial pending apps code

---
+++ features/step_definitions/pending_completed_applications.rb
@@ -0,0 +1,54 @@
+Given(/^pending applications exist$/) do
+  # Andy is writing a function for this
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^completed applications exist$/) do
+  # Andy is writing a function for this
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I elect to view requests$/) do
+  #click_link
+end
+
+Then(/^a separate list of pending requests followed by completed requests are shown in order of receipt by date & time$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^each request shows the details of the change$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the correct data is displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a list of pending requests are shown in order of receipt by date & time$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a separate list of completed requests are shown in order of receipt by date & time$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a view requests option is not displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am not the proprietor of a registered title$/) do
+    step "a registered title with characteristics", ''
+    #do not link title to email i.e. this title will not be owned by this propritor
+end
+
+Given(/^a view requests option is not displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I amend the url to directly go to the pending screen$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^an error message is displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
