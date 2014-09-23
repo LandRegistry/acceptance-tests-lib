03c15f3648d09bc885f5b687f84fb0ce103b631b
Carrie1910 <carrie.sutton@digital.landregistry.gov.uk>
2014-09-23 11:02:39 +0100

    latest changes

--- features/step_definitions/pending_completed_applications.rb
+++ features/step_definitions/pending_completed_applications.rb
@@ -1,26 +1,11 @@
 Given(/^pending applications exist$/) do
-  $pending_cases = []
-
-  $marriage_data = create_marriage_data('GB', $regData['proprietors'][0]['full_name'])
-  $pending_cases << create_change_of_name_marriage_request($regData, $marriage_data)
-
-  $marriage_data = create_marriage_data('GB', $regData['proprietors'][0]['full_name'])
-  $pending_cases << create_change_of_name_marriage_request($regData, $marriage_data)
-
+  # Andy is writing a function for this
+  pending # express the regexp above with the code you wish you had
 end
 
 Given(/^completed applications exist$/) do
-
-  $completed_cases = []
-
-  $marriage_data = create_marriage_data('GB', $regData['proprietors'][0]['full_name'])
-  $completed_cases << create_change_of_name_marriage_request($regData, $marriage_data)
-  complete_case($completed_cases[0]['case_id'])
-
-  $marriage_data = create_marriage_data('GB', $regData['proprietors'][0]['full_name'])
-  $completed_cases << create_change_of_name_marriage_request($regData, $marriage_data)
-  complete_case($completed_cases[1]['case_id'])
-
+  # Andy is writing a function for this
+  pending # express the regexp above with the code you wish you had
 end
 
 When(/^I elect to view requests$/) do
@@ -28,8 +13,10 @@ When(/^I elect to view requests$/) do
 end
 
 Then(/^a separate list of pending requests followed by completed requests are shown in order of receipt by date & time$/) do
-  pending # express the regexp above with the code you wish you had
+  assert_match('Pending changes', page.body, 'Expected to find 'Pending changes' text displayed on the screen')
+  assert_match('Previous changes', page.body, 'Expected to find 'Previous changes' text displayed on the screen')
 end
+
 
 Then(/^each request shows the details of the change$/) do
   pending # express the regexp above with the code you wish you had
