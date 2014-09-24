b195e39c08725bdd85e4b91157f44b56b0798dd3
Nicolas Devetak <nicolasdevetak@gds3619.local>
2014-09-24 15:54:43 +0100

    error message for no results has changed

--- features/step_definitions/citizen_find_a_property.rb
+++ features/step_definitions/citizen_find_a_property.rb
@@ -11,5 +11,5 @@ Given(/^I search for a property on gov\.uk that does not exist$/) do
 end
 
 Then(/^I get a no results are found message$/) do
-  assert_match('No results found', page.body, 'Expected an error message saying no results found, however this wasn\'t present')
+  assert_match('Sorry, no results found', page.body, 'Expected an error message saying no results found, however this wasn\'t present')
 end
