86f73d20fe8b7f11fabea5738cc99050a35420ac
Andy Moore <andy.moore@digital.landregistry.gov.uk>
2014-09-26 07:31:36 +0100

    Removed Performance Code

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,3 @@
-
-
 Before do | scenario |
   if page.driver.respond_to?(:basic_auth)
     page.driver.basic_auth($http_auth_name, $http_auth_password)
@@ -13,29 +11,10 @@ Before do | scenario |
   if (ENV['WEBDRIVER'] != 'Firefox') then
     page.driver.add_header("Referer", "", permanent: true)
   end
-  page.driver.clear_network_traffic
-
-  $step = 0
-
-  $function_call_name = []
-  $function_call_data = []
-  $function_call_arguments = []
-  $function_call_start = 0
-
-  $file_not_created = true
 end
 
 After do | scenario |
   if (scenario.failed?)
       save_screenshot("sshot-#{Time.new.to_i}.png", :full => true)
   end
-
-end
-
-
-AfterStep do | scenario |
-  if (!ENV['GENERATE_PERFORMANCE_SCRIPT'].nil?) then
-    generate_performance_test_script(scenario)
-  end
-
 end
