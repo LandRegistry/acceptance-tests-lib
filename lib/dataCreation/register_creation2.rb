f88cd8e797fd8ee41619a221504f14d02ba7d53f
Nicolas Devetak <nicolasdevetak@gds3619.local>
2014-10-22 15:48:06 +0100

    a little tidy up

--- lib/dataCreation/register_creation2.rb
+++ lib/dataCreation/register_creation2.rb
@@ -153,64 +153,53 @@ end
 
 def add_restrictive_covenants(regData)
   regData['restrictive_covenants'][0] = Hash.new()
-  regData['restrictive_covenants'][0]['template'] = ""
-  regData['restrictive_covenants'][0]['full_text'] = ""
+  regData['restrictive_covenants'][0]['template'] = "By an Order of the Upper Tribunal (Lands Chamber) dated *DA* made pursuant to Section 84 of the Law of Property Act 1925 the restrictive covenants contained in the *DT**DE* dated *DD* referred to above were released. *N< NOTE: Copy Order filed>N*."
+  regData['restrictive_covenants'][0]['full_text'] = "By an Order of the Upper Tribunal (Lands Chamber) dated 14/06/2013 made pursuant to Section 84 of the Law of Property Act 1925 the restrictive covenants contained in the Conveyance dated 01.06.1996 referred to above were released. NOTE: Copy Order filed"
   regData['restrictive_covenants'][0]['fields'] = Hash.new()
   regData['restrictive_covenants'][0]['deeds'] = Array.new()
   regData['restrictive_covenants'][0]['notes'] = Array.new()
-  regData['restrictive_covenants'][0]['template'] = "By an Order of the Upper Tribunal (Lands Chamber) dated *DA* made pursuant to Section 84 of the Law of Property Act 1925 the restrictive covenants contained in the *DT**DE* dated *DD* referred to above were released. *N< NOTE: Copy Order filed>N*."
-  regData['restrictive_covenants'][0]['full_text'] = "By an Order of the Upper Tribunal (Lands Chamber) dated 14/06/2013 made pursuant to Section 84 of the Law of Property Act 1925 the restrictive covenants contained in the Conveyance dated 01.06.1996 referred to above were released. NOTE: Copy Order filed"
   return regData
 end
 
 def add_restriction(regData)
   regData['restrictions'][0] = Hash.new()
-  regData['restrictions'][0]['template'] = ""
-  regData['restrictions'][0]['full_text'] = ""
-  regData['restrictions'][0]['fields'] = Hash.new()
-  regData['restrictions'][0]['fields']['miscellaneous'] = ""
-  regData['restrictions'][0]['deeds'] = Array.new()
-  regData['restrictions'][0]['notes'] = Array.new()
   regData['restrictions'][0]['template'] = "RESTRICTION: No disposition by the proprietor of the registered estate or in exercise of the power of sale or leasing in any registered charge (except an exempt disposal as defined by section 81(8) of the Housing Act 1988) is to be registered without the consent of - (a) in relation to a disposal of land in England by a private registered provider of social housing, the Regulator of Social Housing, (b) in relation to any other disposal of land in England, the Secretary of State, and (c) in relation to a disposal of land in Wales, the Welsh Ministers, to that disposition under *M<>M*."
   regData['restrictions'][0]['full_text'] = "RESTRICTION: No disposition by the proprietor of the registered estate or in exercise of the power of sale or leasing in any registered charge (except an exempt disposal as defined by section 81(8) of the Housing Act 1988) is to be registered without the consent of - (a) in relation to a disposal of land in England by a private registered provider of social housing, the Regulator of Social Housing, (b) in relation to any other disposal of land in England, the Secretary of State, and (c) in relation to a disposal of land in Wales, the Welsh Ministers, to that disposition under section 133 of that Act."
+  regData['restrictions'][0]['fields'] = Hash.new()
   regData['restrictions'][0]['fields']['miscellaneous'] = "section 133 of that Act"
+  regData['restrictions'][0]['deeds'] = Array.new()
+  regData['restrictions'][0]['notes'] = Array.new()
   return regData
 end
 
 def add_bankruptcy(regData)
   regData['bankruptcy'][0] = Hash.new()
-  regData['bankruptcy'][0]['template'] = ""
-  regData['bankruptcy'][0]['full_text'] = ""
+  regData['bankruptcy'][0]['template'] = "BANKRUPTCY NOTICE entered under section 86(2) of the Land Registration Act 2002 in respect of a pending action, as the title of the proprietor of the registered estate appears to be affected by a petition in bankruptcy against *NM* presented in the *M<>M* Court (Court Reference Number *M<>M*) (Land Charges Reference Number PA*M<>M*)."
+  regData['bankruptcy'][0]['full_text'] = "BANKRUPTCY NOTICE entered under section 86(2) of the Land Registration Act 2002 in respect of a pending action, as the title of the proprietor of the registered estate appears to be affected by a petition in bankruptcy against James Lock presented in the Gloucester County Court (Court Reference Number 124578) (Land Charges Reference Number PA102)."
   regData['bankruptcy'][0]['fields'] = Hash.new()
   regData['bankruptcy'][0]['deeds'] = Array.new()
   regData['bankruptcy'][0]['notes'] = Array.new()
-  regData['bankruptcy'][0]['template'] = "BANKRUPTCY NOTICE entered under section 86(2) of the Land Registration Act 2002 in respect of a pending action, as the title of the proprietor of the registered estate appears to be affected by a petition in bankruptcy against *NM* presented in the *M<>M* Court (Court Reference Number *M<>M*) (Land Charges Reference Number PA*M<>M*)."
-  regData['bankruptcy'][0]['full_text'] = "BANKRUPTCY NOTICE entered under section 86(2) of the Land Registration Act 2002 in respect of a pending action, as the title of the proprietor of the registered estate appears to be affected by a petition in bankruptcy against James Lock presented in the Gloucester County Court (Court Reference Number 124578) (Land Charges Reference Number PA102)."
   return regData
 end
 
 def add_easement(regData)
   #to generate an easement - genenerate_title_easement2($regData['extent'], {'has a polygon with easement' => true})
   regData['easements'][0] = Hash.new()
-  regData['easements'][0]['template'] = ""
-  regData['easements'][0]['full_text'] = ""
+  regData['easements'][0]['template'] = "The land *E<>E* is subject to the rights granted by a *DT**DE* dated *DD* made between *DP*. The said Deed also contains restrictive covenants by the grantor. *N<^NOTE: Copy in Certificate. Copy filed>N*."
+  regData['easements'][0]['full_text'] = "The land tinted pink is subject to the rights granted by a Deed dated 03.03.1976 made between Mr Michael Jones and Mr Jeff Smith. The said Deed also contains restrictive covenants by the grantor. NOTE: Copy filed."
   regData['easements'][0]['fields'] = Hash.new()
   regData['easements'][0]['deeds'] = Array.new()
   regData['easements'][0]['notes'] = Array.new()
-  regData['easements'][0]['template'] = "The land *E<>E* is subject to the rights granted by a *DT**DE* dated *DD* made between *DP*. The said Deed also contains restrictive covenants by the grantor. *N<^NOTE: Copy in Certificate. Copy filed>N*."
-  regData['easements'][0]['full_text'] = "The land tinted pink is subject to the rights granted by a Deed dated 03.03.1976 made between Mr Michael Jones and Mr Jeff Smith. The said Deed also contains restrictive covenants by the grantor. NOTE: Copy filed."
   return regData
 end
 
 def add_provision(regData)
   regData['provisions'][0] = Hash.new()
-  regData['provisions'][0]['template'] = ""
-  regData['provisions'][0]['full_text'] = ""
+  regData['provisions'][0]['template'] = "A *DT**DE* dated *DD* made between *DP* contains the following provision:-*VT*"
+  regData['provisions'][0]['full_text'] = "A Transfer of the land in this title dated 01.06.1996 made between Mr Michael Jones and Mr Jeff Smith contains the following provision:-The land has the benefit of a right of way along the passageway to the rear of the property, and also a right of way on foot only on to the open ground on the north west boundary of the land in this title"
   regData['provisions'][0]['fields'] = Hash.new()
   regData['provisions'][0]['deeds'] = Array.new()
   regData['provisions'][0]['notes'] = Array.new()
-  regData['provisions'][0]['template'] = "A *DT**DE* dated *DD* made between *DP* contains the following provision:-*VT*"
-  regData['provisions'][0]['full_text'] = "A Transfer of the land in this title dated 01.06.1996 made between Mr Michael Jones and Mr Jeff Smith contains the following provision:-The land has the benefit of a right of way along the passageway to the rear of the property, and also a right of way on foot only on to the open ground on the north west boundary of the land in this title"
   return regData
 end
 
@@ -224,32 +213,29 @@ end
 
 def add_charge(regData)
   regData['charges'][0] = Hash.new()
-  regData['charges'][0]['template'] = ""
-  regData['charges'][0]['full_text'] = ""
+  regData['charges'][0]['template'] = "CHARGE"
+  regData['charges'][0]['full_text'] = "CHARGE full text"
   regData['charges'][0]['fields'] = Hash.new()
   regData['charges'][0]['deeds'] = Array.new()
   regData['charges'][0]['notes'] = Array.new()
-  regData['charges'][0]['template'] = "CHARGE"
-  regData['charges'][0]['full_text'] = "CHARGE full text"
+
   return regData
 end
 
 def add_other(regData)
   regData['other'][0] = Hash.new()
-  regData['other'][0]['template'] = ""
-  regData['other'][0]['full_text'] = ""
+  regData['other'][0]['template'] = "OTHER"
+  regData['other'][0]['full_text'] = "OTHER full text"
   regData['other'][0]['fields'] = Hash.new()
   regData['other'][0]['deeds'] = Array.new()
   regData['other'][0]['notes'] = Array.new()
-  regData['other'][0]['template'] = "OTHER"
-  regData['other'][0]['full_text'] = "OTHER full text"
   return regData
 end
 
 def add_h_schedule(regData)
   regData['h_schedule'] = Hash.new()
-  regData['h_schedule']['template'] = ""
-  regData['h_schedule']['full_text'] = ""
+  regData['h_schedule']['template'] = "h_schedule here"
+  regData['h_schedule']['full_text'] = "h_schedule here"
   regData['h_schedule']['fields'] = Hash.new()
   regData['h_schedule']['deeds'] = Array.new()
   regData['h_schedule']['notes'] = Array.new()
